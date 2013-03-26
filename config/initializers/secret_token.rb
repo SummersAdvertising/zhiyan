a894c8ee29166e1fb863ed39e2cd85676a10f48a
Yuzhe <yuzhe.c85@gmail.com>
2013-03-26 14:39:57 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhiyan::Application.config.secret_token = '554436d7c3db18ac8da3ecf7fa6b665e37b5d1ec7275346feba26d40a2d6575149e4a2856546ce925b4c991c47062717eba5c7bf25ed00db00eb6af38f74fa95'
